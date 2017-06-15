205556054190e5d3c611fa91db4e04e459bb2899
cmg1eroles <cmg1eroles@gmail.com>
2017-06-15 21:39:43 +0800

    test lang XD

--- Trial.js
+++ Trial.js
@@ -1,8 +1,13 @@
 var root = 'https://jsonplaceholder.typicode.com';
 
-$.ajax({
-  url: root + '/posts/1',
-  method: 'GET'
-}).then(function(data) {
-  console.log(data);
+$(document).ready(function() {
+	$.ajax({
+	  url: root + '/posts/1',
+	  method: 'GET'
+	}).then(function(data) {
+		$("#puname1").text(data.username)
+		$("#ptitle1").text(data.title + " | ");
+		$("#pbody1").text(data.body);
+	  //console.log(data.body);
+	});
 });
